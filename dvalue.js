609ebe7d33348ab2e66f3addaae7feee79c8bef3
hasharas <hasharamsankalpam@gmail.com>
2025-03-09 19:00:23 +0530

    print 1-100 num

---
+++ dvalue.js
@@ -0,0 +1,41 @@
+// const NewArr = [1, 3, 5, 23, 2];
+
+// const value = Math.max(...NewArr);
+
+// console.log("value :", value);
+
+// const NewArr = [13, 20, 3, 4];
+
+// const value = Math.max(...NewArr);
+
+// console.log("Value is :", value)
+
+
+// const NewArr = [ "hashara","kasun","ruwan"];
+
+// const name = ()=> {
+
+// } 
+
+// const numbers = [1, 2, 3, 4, 5, 4, 8, 3]; //sort method
+// numbers.sort((a, b) => b - a);
+// const max = numbers[0];
+// console.log(max);
+
+
+// const names = numbers[0];
+
+// const numb = [1, 2, 3, 4, 5, 3];
+
+// const max = Math.max(...numb);
+
+// console.log("Max val : ", max);
+
+const number = [1, 4, 2, 3, 6, , 9];
+number.sort((a, b) => b - a);
+const value = number[0];
+console.log("Max val : ", value);
+
+for (let i = 1; i < 100; i++) {
+      console.log(i);
+}
