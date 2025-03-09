24dd6a968e1e11b774273aefa9423d1158d1f9e6
hasharas <hasharamsankalpam@gmail.com>
2025-03-09 23:31:57 +0530

    find max val

--- print1-100.js
+++ print1-100.js
@@ -2,8 +2,27 @@
 //       console.log(i);
 // }
 
-for (let i = 1; i < 100; i++) {
-      if (i % 2 !== 0) {
-            console.log("odd num :", i);
+// for (let i = 1; i < 100; i++) {
+//       if (i % 2 !== 0) {
+//             console.log("odd num :", i);
+//       }
+// }
+
+for (let i = 0; i < 100; i++) {
+
+      if (i % 2 === 0) {
+            console.log(i);
       }
 }
+
+const numbers = [1, 2, 3, 4, 5, 6, 5, 4];
+
+// const max = Math.max(...numbers);
+// console.log("max value :", max);
+
+// numbers.sort((a, b) => b - a);
+// const max = numbers[0];
+// console.log("max val :", max);
+
+const max = numbers[0];
+
